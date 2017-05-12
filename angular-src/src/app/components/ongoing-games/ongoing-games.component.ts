a2ae39ce1d8f8a3e552138395d89e34412e3a1dd
AndreiDogaru <andrei_dogaru12@yahoo.com>
2017-05-12 13:03:24 +0200

    layout done

--- angular-src/src/app/components/ongoing-games/ongoing-games.component.ts
+++ angular-src/src/app/components/ongoing-games/ongoing-games.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ongoing-games.component.scss']
 })
 export class OngoingGamesComponent implements OnInit {
+  private games: Object[];
 
   constructor() { }
 
   ngOnInit() {
+    this.games = [
+      {"player1":"Test","player2":"Haha","score1":1,"score2":1},
+      {"player1":"Petru","player2":"Boy","score1":1,"score2":2},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"Miau","player2":"Blabla","score1":0,"score2":1},
+      {"player1":"el","player2":"ea","score1":0,"score2":0}
+    ];
   }
 
 }
