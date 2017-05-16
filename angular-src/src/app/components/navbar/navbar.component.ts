896cc9774dee2dacd6ed0a04d320a885b0aa9647
Petru Birzu <petru.birzu96@gmail.com>
2017-05-16 18:22:26 +0200

    mobile navbar collapse on navigation

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
@@ -7,16 +7,35 @@ import { FlashMessagesService } from 'angular2-flash-messages';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
-  constructor(private flashMessage: FlashMessagesService,
-              private router: Router) { }
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private router: Router,
+    private el: ElementRef
+  ) { }
 
   ngOnInit() {
   }
 
-  onLogoutClick(){
+  onLogoutClick(): boolean {
+    this.onNavigate();
     console.log('log out');
+
+    return false;
+  }
+
+  onNavigate(): void {
+    let butt = this.el.nativeElement.querySelector(".navbar-toggle");
+    let isCollapsed = this.hasClass(butt, "collapsed");
+    if (isCollapsed == false) {
+      butt.click();
+    }
+  }
+
+  hasClass(element, cls): boolean {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
   }
 
 }
