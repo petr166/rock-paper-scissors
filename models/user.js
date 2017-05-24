2eecdad8ba5285a16ec1da2a959b759dc2c1d49b
Petru Birzu <petru.birzu96@gmail.com>
2017-05-24 14:25:11 +0200

    merge database: added routes and functionality for manipulatiing the User data

--- models/user.js
+++ models/user.js
@@ -39,20 +39,20 @@ const UserSchema = mongoose.Schema({
 });
 
 // static functions
-UserSchema.statics.getById = (id, callback) => {
+UserSchema.statics.getUserById = (id, callback) => {
   User.findById(id, callback);
 };
 
-UserSchema.statics.getByUsername = (username, callback) => {
+UserSchema.statics.getUserByUsername = (username, callback) => {
   let query = {username: username};
   User.findOne(query, callback);
 };
 
 UserSchema.statics.addUser = (userToAdd, callback) => {
-  User.getByUsername(userToAdd.username, (err, user) => {
+  User.getUserByUsername(userToAdd.username, (err, user) => {
     if (err) {
-      let error = {msg: "There was a problem searching for the user"};
-      callback(error);
+      console.error(err);
+      callback(err);
       return;
     }
     if (user) { // duplicate username
@@ -63,24 +63,136 @@ UserSchema.statics.addUser = (userToAdd, callback) => {
       // hash the password
       bcryptjs.genSalt(10, (err, salt) => {
         if (err) {
-          let error = {msg: "There was an error registering the new user"};
-          callback(error);
+          console.error(err);
+          callback(err);
           return;
         }
         bcryptjs.hash(userToAdd.password, salt, (err, hash) => {
           if (err) {
-            let error = {msg: "There was an error registering the new user"};
-            callback(error);
+            console.error(err);
+            callback(err);
             return;
           }
 
           userToAdd.password = hash;
-          userToAdd.save(callback); // db save
+          userToAdd.save((err, addedUser) => { // db save
+            if (err || !addedUser) {
+              console.error(err);
+              callback(err);
+              return;
+            } else {
+              addedUser.password = "!private";
+              callback(null, addedUser);
+              return;
+            }
+          });
         });
       });
     }
   });
 };
+
+UserSchema.statics.removeUser = (id, callback) => {
+  User.findByIdAndRemove(id, (err, user) => {
+    if (err) {
+      console.error(err);
+      callback(err);
+      return;
+    } else if (!user) {
+      let error = {msg: "The user account could not be found"};
+      callback(error);
+      return;
+    } 
+    else {
+      user.password = "!private";
+      callback(null, user);
+      return;
+    }
+  });
+};
+
+UserSchema.statics.authenticateUser = (username, password, callback) => {
+  User.getUserByUsername(username, (err, user) => {
+    if (err || !user) {
+      let error = {msg: "Wrong username or password"};
+      callback(error);
+      return;
+    }
+    else {
+      bcryptjs.compare(password, user.password, (err, result) => {
+        if (err) {
+          console.error(err);
+          callback(err);
+          return;
+        }
+        if (result == true) {
+          user.password = "!private";
+          callback(null, user);
+          return;
+        } else {
+          let error = {msg: "Wrong username or password"};
+          callback(error);
+          return;
+        }
+      });
+    }
+  });
+};
+
+UserSchema.statics.updateUserPassword = (id, oldPass, newPass, callback) => {
+  User.getUserById(id, (err, user) => {
+    if (err || !user) {
+      let error = {msg: "The user could not be found"};
+      callback(error);
+      return;
+    } else {
+      bcryptjs.compare(oldPass, user.password, (err, result) => { // check the old password authenticity
+        if (err) {
+          console.error(err);
+          callback(err);
+          return;
+        }
+
+        if (result == true) { // the old pass is correct, update it!
+          // hash the password
+          bcryptjs.genSalt(10, (err, salt) => {
+            if (err) {
+              console.error(err);
+              callback(err);
+              return;
+            }
+            bcryptjs.hash(newPass, salt, (err, hash) => {
+              if (err) {
+                console.error(err);
+                callback(err);
+                return;
+              }
+
+              user.password = hash;
+              user.save((err, updatedUser) => { // db save
+                if (err || !updatedUser) {
+                  console.error(err);
+                  callback(err);
+                  return;
+                } else {
+                  updatedUser.password = "!private";
+                  callback(null, updatedUser);
+                  return;
+                }
+              });
+            });
+          });
+        }
+        else {
+          let error = {msg: "The old password does not match"};
+          callback(error);
+          return;
+        }
+      });
+    }
+  });
+};
+
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const config = require('../config/database');
+const jwt = require('jsonwebtoken');
 
 
 // POST "/register"
@@ -21,7 +23,7 @@ router.post("/register", (req, res, next) => {
 
   User.addUser(newUser, (err, addedUser) => {
     if (err) {
-      response.msg = err.msg || "Failed to register user";
+      response.msg = err.msg || "There was an error registering the new user";
       res.json(response);
     } else {
       console.log("[%s] registered successfuly", addedUser.username);
@@ -33,6 +35,104 @@ router.post("/register", (req, res, next) => {
       res.json(response);
     }
   });
+});
+
+
+// POST "/login"
+router.post("/login", (req, res, next) => {
+  let body = req.body;
+  let response = {success: false};
+
+  User.authenticateUser(body.username.trim(), body.password.trim(), (err, user) => {
+    if (err || !user) {
+      response.msg = err.msg || "There was an error authenticating the user";
+      res.json(response);
+    } else { // generate the unique token to be sent to the user
+      let token = jwt.sign(user, config.secret, {expiresIn: 604800});
+
+      response.token = "JWT " + token;
+      response.user = user;
+      response.msg = "User authenticated successfuly";
+      response.success = true;
+
+      console.log("[%s] authenticated successfuly", user.username);
+      res.json(response);
+    }
+  });
+});
+
+
+// GET "/profile/:id"
+router.get("/profile/:id", (req, res, next) => {
+  let theId = req.params.id;
+  let response = {success: false};
+
+  User.getUserById(theId, (err, user) => {
+    if (err || !user) {
+      response.msg = "The user could not be found";
+      res.json(response);
+    } else {
+      response.success = true;
+      response.msg = "User profile retrieved successfuly";
+      user.password = "!private";
+      response.user = user;
+
+      res.json(response);
+    }
+  });
+});
+
+
+// POST "/update-password"
+router.post("/update-password", (req, res, next) => {
+  let body = req.body;
+  let response = {success: false};
+
+  // TODO make sure the requester tries to update his own pass
+
+  if (body.oldPass == body.newPass) {
+    response.msg = "The new password must not match the old one";
+    res.json(response);
+  }
+  else {
+    User.updateUserPassword(body.id, body.oldPass, body.newPass, (err, updatedUser) => {
+      if (err) {
+        response.msg = err.msg || "There was an error updating the password";
+        res.json(response);
+      } else {
+        response.success = true;
+        response.msg = "Password updated successfuly";
+        response.user = updatedUser;
+
+        console.log("[%s] updated his password", updatedUser.username);
+        res.json(response);
+      }
+    });
+  }
+});
+
+
+// POST "/remove"
+router.post("/remove", (req, res, next) => {
+  let body = req.body;
+  let response = {success: false};
+
+  // TODO make sure the requester tries to remove his own account
+
+  User.removeUser(body.id, (err, removedUser) => {
+    if (err || !removedUser) {
+      response.msg = err.msg || "There was an error removing the account";
+      res.json(response);
+    } else {
+      response.success = true;
+      response.msg = "Account removed successfuly";
+      response.user = removedUser;
+
+      console.log("[%s] removed his account", removedUser.username);
+      res.json(response);
+    }
+  });
+
 });
 
 
